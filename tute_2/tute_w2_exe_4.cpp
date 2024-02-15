7f43be2d29bf8b33b607d5258d8e20d4c8231a3a
Senrith Fernando <115388481+SenrithFernando@users.noreply.github.com>
2024-02-15 08:05:12 +0530

    Update tute_w2_exe_4.cpp

--- tute_2/tute_w2_exe_4.cpp
+++ tute_2/tute_w2_exe_4.cpp
@@ -6,16 +6,16 @@ int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
 
- cout << "No" << "Name" << "Marks" << endl;
+ cout << setw(5)<<"No"<<setw(10) << "Name" <<setw(9)<< "Marks" << endl;
 
  for (int r = 0; r < 5; r++) {
-     cout << r+1<< setw(5)
-     << names[r]
+     cout<< setw(5)<< r+1<< setw(10)
+     << names[r] <<setw(9)
      << marks[r] << endl;
  }
 
 
-cout<<setw(5), setprecision(15), setiosflags(ios::fixed);
+//cout<<setw(5), setprecision(15), setiosflags(ios::fixed);
 //cout<<setiosflags(ios::fixed)<<setprecision(15)<<setw(5)<<25.12347<<endl;
 }
 
