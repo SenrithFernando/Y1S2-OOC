6b004edf7b061a3cdbe7941d9feb5024d4ab4e6d
Senrith Fernando <rihansenrithvta@gmail.com>
2024-02-27 21:32:11 +0530

    crete lab 2

--- lab/lab_2/lab_2_exe_1.cpp
+++ lab/lab_2/lab_2_exe_1.cpp
@@ -1,50 +1,87 @@
 #include <iostream>
-#include<iomanip>
+#include <iomanip>
 using namespace std;
 int searchKey(int search);
 int findMax(char suggest);
 
-float numbers[10]={10,54,75,84,48,76,68,24,16,35};
+int numbers[10];
+//= { 10,54,75,84,48,76,68,24,16,35 }
 
-int main() {
-
+int main()
+{
     int r;
+    float arra;
     char sug;
 
+    for (int i; i < 10; i++)
+    {
+        std::cout << "Enter  value  " << i << " :";
+        std::cin >> arra;
 
-    std::cout<<"Enter Search a value  ";
-    std::cin>>r;
+        numbers[i] = arra;
+    }
 
-    std::cout<<"value : ";
-    std::cout<<searchKey(r);
+    std::cout << "Enter search value : ";
+    std::cin >> r;
 
-    std::cout<<"Want max value  (y/n) :";
-    std::cin>>sug;
+    std::cout << "value : ";
+    std::cout << searchKey(r) << endl;
 
-    std::cout<<"Max value : ";
-    std::cout<<findMax(sug);
+    std::cout << "Want max value  (y/n) :";
+    std::cin >> sug;
+
+    std::cout << "Max value : ";
+    std::cout << findMax(sug) << endl;
 }
 
-int searchKey(int search) {
-    return numbers[search-1];
-}
+int searchKey(int search)
+{
 
-
-int findMax(char suggest){
-    int x=0;
-    if(suggest=='y' || suggest=='Y'){
-        for(int i=0; i<=10; i++){
-            if(x<numbers[i]){
-                x=numbers[i];
-            }else{
-                x=x;
+    if (search == -1)
+    {
+        std::cout << "Value Not fount";
+        return -1;
+    }
+    else
+    {
+        for (int i; i < 10; i++)
+        {
+            if (numbers[i] == search)
+            {
+                return i;
+            }
+            else
+            {
+                std::cout << "Value Not fount";
+                return -1;
             }
         }
-    }else{
+        // return numbers[search - 1];
+    }
+    return -1;
+}
+
+int findMax(char suggest)
+{
+    int x = 0;
+    if (suggest == 'y' || suggest == 'Y')
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            if (x < numbers[i])
+            {
+                x = numbers[i];
+            }
+            else
+            {
+                x = x;
+            }
+        }
+    }
+    else
+    {
         return 0;
     }
 
     return x;
 }
-
-
---
+++ lab/lab_2/lab_2_exe_2_other.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+// int area(int length, int width);
+
+struct Rectangle
+{
+    int length;
+    int height;
+};
+
+float area(Rectangle r);
+
+int main(void)
+{
+    struct Rectangle r1;
+    struct Rectangle r2;
+
+    int Yar, Ywid, Har, Hwid;
+
+    std::cout << "Enter Width of yard : ";
+    std::cin >> r1.length;
+
+    std::cout << "Enter Width of yard : ";
+    std::cin >> r1.height;
+
+    std::cout << "Enter Width of house : ";
+    std::cin >> r2.length;
+
+    std::cout << "Enter Width of house : ";
+    std::cin >> r2.height;
+
+    // std::cout << area(Ywid,Yar)- area(Har,Hwid) << endl;
+    std::cout << "Area of green color : ";
+    // std::cout<< area(r1)- area(r2) << endl;
+    cout << "" << setiosflags(ios::fixed) << setprecision(3) << area(r1) - area(r2) << endl;
+}
+
+float area(Rectangle r)
+{
+
+    return r.length * r.height;
+}
